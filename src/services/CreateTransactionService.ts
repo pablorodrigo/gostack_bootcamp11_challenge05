5edd90a41855445057296520780a0251e3057cc6
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-04-25 16:10:48 -0400

    [NodeJS][Challenge05] - Created register and list of the transations feature

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,12 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Please select if it is a income or outcome');
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
